591e0495d232144c09d16abee1aa07a8f57a96a1
Robert Niederreiter <office@squarewave.at>
2013-02-23 18:52:48 +0100

    Fix version output of generator listing in ``IConfLoader.generators``.

--- README.rst
+++ README.rst
@@ -3,6 +3,13 @@ This Package is part of **AGX**. See `<http://agx.me>`_ for Details.
 Changes
 =======
 
+1.0a2
+-----
+
+- Fix version output of generator listing in ``IConfLoader.generators``.
+  [rnix, 2013-02-23]
+
+
 1.0a1
 -----
 
--- src/agx/core/config.py
+++ src/agx/core/config.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import ConfigParser
+import pkg_resources
 from StringIO import StringIO
 
 from zope.interface import implementer
@@ -26,17 +27,9 @@ class ConfLoader(object):
     @property
     def generators(self):
         ret = list()
-        cmd = 'git rev-parse --verify HEAD'
         for generator in generators:
-            directory = os.path.split(generator.__file__)
-            while directory[1] != 'src':
-                directory = os.path.split(directory[0])
-            os.chdir(directory[0])
-            p = subprocess.Popen(
-                cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE, close_fds=True)
-            output = p.stdout.readlines()
-            version = output[0].strip('\n')
+            name = generator.__name__
+            version = pkg_resources.get_distribution(name).version
             ret.append((generator.__name__, version))
         return ret
 
